0613d7eada046f1c3464b5c3fd787c608a47b026
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2023-05-23 09:42:42 -0400

    Move debug search functions in DebugUtils.

--- Source/ui_qt/DebugSupport/DebugUtils.h
+++ Source/ui_qt/DebugSupport/DebugUtils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <QFont>
 #include "MIPS.h"
 #include "BiosDebugInfoProvider.h"
@@ -9,5 +10,7 @@ namespace DebugUtils
 {
 	std::string PrintAddressLocation(uint32, CMIPS*, const BiosDebugModuleInfoArray&);
 	const BIOS_DEBUG_MODULE_INFO* FindModuleAtAddress(const BiosDebugModuleInfoArray&, uint32);
+	std::vector<uint32> FindCallers(CMIPS*, uint32);
+	std::vector<uint32> FindWordValueRefs(CMIPS*, uint32, uint32);
 	QFont CreateMonospaceFont();
 }
--- Source/ui_qt/DebugSupport/QtDebugger.h
+++ Source/ui_qt/DebugSupport/QtDebugger.h
@@ -111,10 +111,6 @@ private:
 	CRegViewWnd* GetRegisterViewWindow();
 	CCallStackWnd* GetCallStackWindow();
 
-	//Search functions
-	static std::vector<uint32> FindCallers(CMIPS*, uint32);
-	static std::vector<uint32> FindWordValueRefs(CMIPS*, uint32, uint32);
-
 	//Event handlers
 	void OnFunctionsViewFunctionDblClick(uint32);
 	void OnFunctionsViewFunctionsStateChange();
